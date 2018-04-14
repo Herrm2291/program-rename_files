d472dfee02082042ca96ca69891aba68654222e9
Steve Herrmann <37193114+Herrm2291@users.noreply.github.com>
2018-04-14 11:02:27 -0400

    created function to rename files, added code to get file names list

---
+++ rename_files.py
@@ -0,0 +1,9 @@
+import os
+
+def rename_files():
+    # get file names from a folder
+    file_list = os.listdir(r"C:\Users\herrm\Desktop\dev\rename_files\prank")
+    print(file_list)
+    # for each file, rename filename
+    
+rename_files()
