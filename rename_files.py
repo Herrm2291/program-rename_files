5c369dfdd1a70eb641a243a48eca78bb844c8da2
Steve Herrmann <37193114+Herrm2291@users.noreply.github.com>
2018-04-14 12:19:25 -0400

    added for loop to rename each file

--- rename_files.py
+++ rename_files.py
@@ -1,9 +1,18 @@
 import os
+import re
 
 def rename_files():
     # get file names from a folder
     file_list = os.listdir(r"C:\Users\herrm\Desktop\dev\rename_files\prank")
     print(file_list)
+    saved_path = os.getcwd()
+    print("Current working directory is " +saved_path)
+    os.chdir(r"C:\Users\herrm\Desktop\dev\rename_files\prank")
     # for each file, rename filename
-    
+    for file_name in file_list:
+        print("Old Name - "+file_name)
+        print("New Name - "+re.sub("[0-9]", "", file_name))
+        os.rename(file_name, re.sub("[0-9]", "", file_name))
+    os.chdir(saved_path)
+
 rename_files()
